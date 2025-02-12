f457e0e6f583171c6363a614fbe564d5e5d7c4b0
CodewithEhtisham <siddiquimahtisham@gmail.com>
2025-02-12 01:27:53 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-let name = "john Doe"
-let age= 49
-var city = "karachi"
-let ph_number = 9846321
-let a33 = "hellow world"
-let is_Alive = true
-residence = city
-let id;
-console.log("The type of is_Alive is:",typeof(is_Alive));
-
-console.log("residence")
-console.log(residence);
-console.log(name)
-
-console.table({name , age , city , a33 , ph_number , is_Alive , residence})
-
-console.log("The table of is_alive is:", (typeof is_Alive));
-console.log("The table of name is:", (typeof name))
-console.log("The type of age is:",(typeof age))
-console.log("The type of city is:",(typeof city))
-console.log("The typr of ph_number is:",(typeof ph_number))
-console.log("The type of residence is:",(typeof letid))
-
-
+let x="arijit sing"
+let y="atif aslam"
+if (x==y){
+    console.log("arijit is best")
+}
+else{
+    console.log("atif aslam is goat")
+}
