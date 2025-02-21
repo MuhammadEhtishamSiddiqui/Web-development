54803a667b9d40f1cbb2fef7d17833be337b32c2
CodewithEhtisham <siddiquimahtisham@gmail.com>
2025-02-21 01:51:35 -0800

    Add files via upload

---
+++ loops2.js
@@ -0,0 +1,30 @@
+// for (i=1;i<=10;i++){
+    // console.log(i);
+// }
+// for (let i=10;i>0;i--){
+    // console.log(i);
+// }
+// for (let i=10; i<=20;i+=2){
+//     console.log(i);
+// }
+// let fruits = ["apple", "bnana", "mango", "strawberry", "cherry"]
+// for (let i =0; i<fruits.length; i++){
+//     console.log(`${fruits[i]} is available`);
+// }
+
+// let i = 0
+// while (i<5) {
+//     console.log("hellow");
+//     1++
+// }
+// let i= 8
+// do {
+//     console.log("Hellow");
+// } while (i<6);
+
+let number = [77,384,87,5,87,54,234,98,66]
+sum = 0;
+for (let i= 0; i < number.length; i++){
+    sum += number[i]   //sum=sum+number[i]
+}
+console.log(sum)
